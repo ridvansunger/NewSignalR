02c3327e033312db192c20ef086a4c96a02a70ad
ridvan.sunger <rdvansunger@gmail.com>
2024-09-25 21:21:02 +0300

    Add project files.

---
+++ SignalR.Web/Hubs/ExampleHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Web.Hubs
+{
+    public class ExampleHub : Hub
+    {
+        //bu method js tarafından tetiklenecek,bu metod tetiklenince de js yani client tarafındaki metodu tetikleyecek.
+        //mobil, akıllı tv bu metodu teikleyen herhangi bir şey olabilir.
+        public async Task BroadcastMessageAllClient(string message)
+        {
+            //Clients=> bu metodu kim çağırıyorsa, bana bağlı olan tüm clientlara yağ (Clients.All)
+            await Clients.All.SendAsync("ReceiveMessageForAllClient",message);
+        }
+
+
+    }
+}
