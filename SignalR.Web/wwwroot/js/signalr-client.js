a2630e675c7d506409970c51673b323138f40e25
ridvan.sunger <rdvansunger@gmail.com>
2024-09-25 22:00:09 +0300

    ConnectToHub

---
+++ SignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/examplehub").configureLogging(signalR.LogLevel.Information).build(); //back ile client aynı projede olduğu için program csde ki maphub daki adını verdik.
+    //configureLogging ile log eklemesi yaptık.
+
+
+
+    function start() {
+        connection.start().then(() => console.log("Hub ile bağlantı kuruldu."));
+    }
+
+    try {
+        start();
+
+    } catch (e)
+    {
+        setTimeout(() => start(), 5000); //bağlantı kuramazasa 5 saniye de bir bağlantı kurmaya çalışsın.
+    }
+
+});
